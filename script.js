d6ed9e3d88afb7d7903f17c1265c12d90675ac13
Hannah <hannahamitchell99@gmail.com>
2023-01-25 16:37:11 +0200

    first part of brief

---
+++ script.js
@@ -0,0 +1,30 @@
+import pokemonArray from "./data/pokemon.js";
+
+const cardContainer = document.querySelector(".card-container");
+
+const makePokemonCard = (characterObject) => {
+  let firstLetterCapitals =
+    characterObject.name.charAt(0).toUpperCase() +
+    characterObject.name.slice(1);
+  const cardHTML = `
+    <div class = "card">
+       <img class = "card__image" src=${characterObject.sprite} alt="">
+       <div class = "card__content">
+         <h2 class = "card__heading">${firstLetterCapitals}</h2>
+         <p class = "card__text">${firstLetterCapitals} (#${
+    characterObject.id
+  }) is a
+         ${characterObject.types.join(" & ")} type pokemon.</p>
+       </div>
+     </div>
+ `;
+  return cardHTML;
+};
+
+const populateCards = (characterCards) => {
+  cardContainer.innerHTML = characterCards.map((character) => {
+    return makePokemonCard(character);
+  }).join("");
+};
+
+populateCards(pokemonArray);
